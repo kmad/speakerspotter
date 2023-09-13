421bc551d100cb619b72d40da99bcf8ffe697e9a
kmad <3433032+kmad@users.noreply.github.com>
2023-09-12 21:06:47 -0400

    Working version with mp3 URLs.

--- process_audio.py
+++ process_audio.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from io import BytesIO
+import requests
 from sentence_transformers import SentenceTransformer
 from multiprocessing import Pool
 from dotenv import load_dotenv
@@ -8,11 +10,12 @@ import subprocess
 import replicate
 import argparse
 import logging
+import openai
 import json
 import uuid
 import os
 
-def generate_embeddings(run_id, speakers):
+def generate_embeddings(run_id, speakers_text):
     logging.info("Embedding sentences in transcripts...")
     
     model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -21,18 +24,15 @@ def generate_embeddings(run_id, speakers):
     # TODO: Replace with a more scalable solution
     output_object  = {
             "run_id": run_id,
-            "speakers": list(speakers),
+            "speakers": list(speakers_text.keys()),
             "data" : {}
         }
 
-    for speaker_label in speakers:
-        transcript_file = f"{run_id}/{speaker_label}-TRANSCRIPT.txt"
+    for speaker_label in speakers_text.keys():
 
-        with open(transcript_file, 'r') as f:
-            text = f.read().replace('\n', ' ')
-            sentences = [sentence.strip() for sentence in text.split('. ') if sentence]
+        sentences = [sentence.strip() for sentence in speakers_text[speaker_label].split('. ') if sentence]
 
-        # TODO: Offload to GPU
+        # TODO: Offload to GPU or AI call
         embeddings = model.encode(sentences)
         
         logging.info(f"Generated embeddings: {len(embeddings)}")
@@ -46,103 +46,122 @@ def generate_embeddings(run_id, speakers):
         f.write('\n')
         
     logging.info("All sentences in transcripts embedded.")
-
-def process_speaker(run_id, speaker_label):
-    audio_input_path = f'{run_id}/{speaker_label}.wav'
-    audio_output_path = f'{run_id}/{speaker_label}_16k.wav'
-    transcript_output_path = f'{run_id}/{speaker_label}-TRANSCRIPT' # Whisper will append .txt
-
-    WHISPER_PATH = os.getenv("WHISPER_PATH")
-    MODEL_PATH   = os.getenv("MODEL_PATH")
-
-    # Resample the audio to 16kHz, which is required by whisper
-    subprocess.run(["ffmpeg"
-                    , "-i", audio_input_path
-                    , "-acodec", "pcm_s16le"
-                    , "-ac", "1"
-                    , "-ar", "16000"
-                    , "-threads", "20"
-                    , audio_output_path]
-                    , stdout=subprocess.STDOUT if args.verbose else subprocess.DEVNULL
-                    , stderr=subprocess.STDOUT if args.verbose else subprocess.DEVNULL)
     
-    logging.info(f"Converted audio for speaker {speaker_label} saved to {audio_output_path}")
-     
-    # Delete the original file
-    subprocess.run(["rm", audio_input_path])
-
-    logging.info("Running whisper...")
-
-    subprocess.run([WHISPER_PATH
-                    , "-m"
-                    , MODEL_PATH
-                    , "-f", f"{audio_output_path}"
-                    , "-otxt"
-                    , "-of", f"{transcript_output_path}"]
-                    , stdout=subprocess.STDOUT if args.verbose else subprocess.DEVNULL
-                    , stderr=subprocess.STDOUT if args.verbose else subprocess.DEVNULL
-                    )
-    
-def extract_all_samples_per_speaker(run_id, audio_path):
+def extract_all_samples_per_speaker(run_id):
+    INPUT_PATH = f'{run_id}/input.mp3' 
 
     logging.info("Extracting all samples per speaker...")
     speakers_segments = {}
 
-    rttm_path = f"{run_id}/output.rttm"
-    with open(rttm_path, 'r') as rttm_file:
-        for line in rttm_file:
-            parts = line.split()
-            if len(parts) >= 5:
-                speaker_label = parts[7]
-                start_time = float(parts[3]) * 1000
-                duration = float(parts[4]) * 1000
-                end_time = start_time + duration
-                if speaker_label not in speakers_segments:
-                    speakers_segments[speaker_label] = [(start_time, start_time + duration)]
-                else:
-                    speakers_segments[speaker_label].append((start_time, start_time + duration))
+
+    with open(f'{run_id}/segments.json', 'r') as f:
+        segment_output = json.load(f)
+
+    data = segment_output
+
+    # Iterate over the segments
+    for segment in data['segments']:
+        speaker = segment['speaker']
+        start_time = segment['start']
+        stop_time = segment['stop']
+
+        # Convert timestamps to milliseconds
+        start_time_parts = start_time.split(':')
+        start_time_ms = int(start_time_parts[0]) * 60 * 60 * 1000 + int(start_time_parts[1]) * 60 * 1000 + int(float(start_time_parts[2]) * 1000)
+
+        stop_time_parts = stop_time.split(':')
+        stop_time_ms = int(stop_time_parts[0]) * 60 * 60 * 1000 + int(stop_time_parts[1]) * 60 * 1000 + int(float(stop_time_parts[2]) * 1000)
+
+        # Add the segment to the speaker's list
+        if speaker not in speakers_segments:
+            speakers_segments[speaker] = []
+        speakers_segments[speaker].append((start_time_ms, stop_time_ms))
     
-    #TODO: Support other formats (convert with ffmpeg?)
+
     logging.info("Loading audio file...")
-    original_audio = AudioSegment.from_wav(audio_path)
+    original_audio = AudioSegment.from_mp3(INPUT_PATH)
     
     logging.info("Segmenting audio...")
     speakers_audio = {}
+    speakers_text = {}
+
     for speaker_label, segments in speakers_segments.items():
         for start_time, end_time in segments:
             segment_audio = original_audio[start_time:end_time]
+
+            # Generate transcript for segment_audio
+            # Use openai model
+            temp_filename = "/tmp/temp_audio.mp3"
+            # segment_audio.set_channels(1)
+            # segment_audio.set_frame_rate(16000)
+            segment_audio.export(temp_filename, format="mp3")
+            
+            # Skip if temp_filename is more than 20MB
+            if os.path.getsize(temp_filename) > 20000000:
+                print(f"Skipping segment {start_time}-{end_time} for speaker {speaker_label} because it is too large.")
+                continue
+
+            with open(temp_filename, 'rb') as audio_file:
+                output = openai.Audio.transcribe("whisper-1", audio_file)
+            
+            # Add to speaker's transcript
+            if speaker_label not in speakers_text:
+                speakers_text[speaker_label] = output['text']
+            else:
+                speakers_text[speaker_label] += output['text'] + " "
+
+            # Add audio segments
             if speaker_label not in speakers_audio:
                 speakers_audio[speaker_label] = segment_audio
             else:
                 speakers_audio[speaker_label] += segment_audio
 
     for speaker_label, speaker_audio in speakers_audio.items():
-        output_path = f'{run_id}/{speaker_label}.wav'
+        # Output transcript
+        transcript_path = f'{run_id}/{speaker_label}-TRANSCRIPT.txt'
+        logging.info(f"Exporting transcript for speaker {speaker_label} to {transcript_path}")
+        with open(transcript_path, "w") as f:
+            f.write(speakers_text[speaker_label])
+
+        # Output audio
+        output_path = f'{run_id}/{speaker_label}.mp3'
         logging.info(f"Exporting segment for speaker {speaker_label} to {output_path}")
-        speaker_audio.set_channels(1)
-        speaker_audio.set_frame_rate(16000)
-        speaker_audio.export(output_path, format="wav")
-
-
-    with Pool() as p:
-        p.starmap(process_speaker, [(run_id, speaker_label) for speaker_label in speakers_audio.keys()])
+        # speaker_audio.set_channels(1)
+        # speaker_audio.set_frame_rate(16000)
+        speaker_audio.export(output_path, format="mp3")
         
     logging.info("All audio segments processed.")
 
     logging.info("Generating embeddings...")
-    generate_embeddings(run_id, speakers_audio.keys())
+
+    generate_embeddings(run_id, speakers_text)
     
-def split_audio(run_id, audio_path):
-    output = replicate.run(
-    "lucataco/speaker-diarization:718182bfdc7c91943c69ed0ac18ebe99a76fdde67ccd01fced347d8c3b8c15a6",
-    input={"audio": open(audio_path, "rb")}
+def diarize_audio(run_id, audio_path):
+ 
+    logging.info("Starting diarization...")
+
+    # Use replicate as the hosted diarization solution
+    output_location = replicate.run(
+        "lucataco/speaker-diarization:718182bfdc7c91943c69ed0ac18ebe99a76fdde67ccd01fced347d8c3b8c15a6",
+        input={"audio": audio_path}
+        #input={"audio": open(audio_path, "rb")}
     )
+    
+    logging.info("Diarization complete.")
+
+    # Replicate saves the results elsewhere
+    r = requests.get(output_location)
+    # Save output to run_id folder
+    with open(f'{run_id}/segments.json', "w") as f:
+        json.dump(r.json(), f)
+    
+    return output_location
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     load_dotenv()
-    
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     # Create an ArgumentParser object
     parser = argparse.ArgumentParser()
 
@@ -157,10 +176,7 @@ if __name__ == "__main__":
 
     # Set the input parameters
     audio_path = args.input
-    # Add error checks for the cli input
-    if not os.path.exists(audio_path):
-        logging.error("Audio file not found: ", audio_path)
-        exit(1)
+
     
     # Create a unique identifier for this run
     run_id = str(uuid.uuid4()) 
@@ -172,9 +188,18 @@ if __name__ == "__main__":
 
     # Use pyannote to extract one sample per speaker
     logging.info("Starting audio processing...")
+    
+    logging.info("Downloading audio file from URL...")
+    
+    r = requests.get(audio_path)
+    with open(f"{run_id}/input.mp3", 'wb') as f:
+        f.write(r.content)
+    
+    # TODO: mp3 vs wav
+    
+    diarize_audio(run_id, audio_path)
 
-    split_audio(run_id, audio_path)
-
-    extract_all_samples_per_speaker(run_id, audio_path)
+    extract_all_samples_per_speaker(run_id)
 
     # TODO: Youtube download / extract / convert to wav
+    # TODO: Speaker detection against existing database using embeddings from segment.json
